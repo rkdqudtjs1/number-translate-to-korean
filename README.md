902fd9c2b7ba3bb007265c6655936014092dac2e
rkdqudtjs1 <rkdqudtjs1@naver.com>
2019-11-26 13:49:43 +0900

    INIT

---
+++ README.md
@@ -0,0 +1,8 @@
+```
+const nttk = require('number-transalte-to-korean')
+const result = nttk(1450000)
+console.log(result)
+
+백사십오만
+
+```
---
+++ index.js
@@ -0,0 +1,20 @@
+const koreanNumbers = value => {
+  const resultValue = []
+  const unit1 = ['', '만', '억', '조']
+  const unit2 = ['', '십', '백', '천', '만']
+  const numData = ['영', '일', '이', '삼', '사', '오', '육', '칠', '팔', '구']
+
+  const valueStr = value + ""
+  const unit1Size = Math.ceil(valueStr.length / 4)
+
+  const splitReverse = valueStr.split('').reverse()
+
+  for (let i = 0; i < unit1Size; i++) {
+    const number = splitReverse.splice(0, 4)
+    const result = number.map((num, idx) => num != 0 ? numData[num] + unit2[idx] : '').reverse().join('') + unit1[i]
+    resultValue.unshift(result)
+  }
+  return resultValue.join('')
+}
+
+module.exports = koreanNumbers
